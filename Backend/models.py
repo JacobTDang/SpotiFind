b66e1a5989e7a4ff5b5a7534b0fbc0da42a4c56b
Jacob Dang <usosempai@gmail.com>
2025-05-29 01:33:07 -0500

    Setup models.py for database migration to use in the future

--- Backend/models.py
+++ Backend/models.py
@@ -1,7 +1,24 @@
 from app import db
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, Boolean, Text
+from sqlalchemy.orm import relationship
+import sqlalchemy as sa
 
 class song(db.Model):
-    __table__ = 'songs'
+    __tablename__ = 'songs'
+    songID = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    artist = Column(String(255), nullable=False)
+    source = Column(String(50), , default = 'local')
     
-    def __repr__(self):
-        return ""
+    
+class songEmbedding(db.Model):
+    __tablename__ = 'songEmbedding'
+    
+    # Set ID's
+    embeddingID = Column(Integer, primary_key=True)
+    songID = Column(Integer, ForeignKey('song.songID'), nullable=False)
+    
+    # Spotify MetaData
+    dimensions = Column(Integer, nullable=False)
+    audioStart = Column(Float, default = 0.0)
+    audioDuration = Column(Float)
